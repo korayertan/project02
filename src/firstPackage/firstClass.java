10d1a95f331e1dcd261d06f37930ab460b9fe810
korayertan <33375980+korayertan@users.noreply.github.com>
2020-11-08 11:55:10 -0500

    Second commit from remote master branch

--- src/firstPackage/firstClass.java
+++ src/firstPackage/firstClass.java
@@ -7,7 +7,7 @@ public class firstClass {
 		System.out.println("second commit from local master branch");
 		System.out.println("third commit from local master branch");
 		System.out.println("first commit from remote master branch");
-		ystem.out.println("Second commit from remote master branch");
+		System.out.println("Second commit from remote master branch");
 
 	}
 
