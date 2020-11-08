bff827f0830037f24ffbdc65b2d22acfd73ab465
Koray <koray@mail.com>
2020-11-08 12:58:44 -0500

    some commit

--- src/firstPackage/firstClass.java
+++ src/firstPackage/firstClass.java
@@ -7,13 +7,14 @@ public class firstClass {
 		System.out.println("second commit from local master branch");
 		System.out.println("third commit from local master branch");
 		System.out.println("first commit from remote master branch");
-		
-		System.out.println("Here is a merge conflict I create from local master");
-
 		System.out.println("Second commit from remote master branch");
 		System.out.println("Third commit from remote master branch");
+		
+		
+		System.out.println("Here is merge conflict I create from local master ");
+		
 		System.out.println("Here is the conflict we have from remote master branch");
-		System.out.println("Here is another conflict from local master branch");
+		
 		System.out.println("Here is another conflict from local master branch");
 
 	}
